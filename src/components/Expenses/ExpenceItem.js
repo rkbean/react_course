18b66931462857deb45f2ec48969e355b6025b1e
Kevin Bean <kevin@ri.local>
2022-02-01 18:05:42 -0700

    dynamic list finished before assignment

--- src/components/Expenses/ExpenceItem.js
+++ src/components/Expenses/ExpenceItem.js
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenceItem.css';
 
 const ExpenceItem = (props) => {
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
   
-  // let title = props.title;
+  // // let title = props.title;
 
-  const clickHandler = () => {
-    setTitle('Updated:');
-    console.log('we were clicked now!! - ' + title);
-  };
+  // const clickHandler = () => {
+  //   setTitle('Updated:');
+  //   console.log('we were clicked now!! - ' + title);
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 };
